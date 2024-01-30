39d94fed905edf355e8bf543c1520f30b1d29288
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-01-30 15:56:19 -0500

    update specs

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,9 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "Dashboards", type: :request do
-  describe "GET /show" do
+  before do
+    @user = create(:user)
+    sign_in @user
+  end
+
+  describe "GET /dashboard" do
     it "returns http success" do
-      get "/dashboard/show"
+      get "/dashboard"
       expect(response).to have_http_status(:success)
     end
   end
